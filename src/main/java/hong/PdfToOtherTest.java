96912c654d280d37bdc3a48c80db1a720a4b1def
GIT-PKH <sidabali79@gmail.com>
2024-02-18 12:49:23 +0900

    이름변경

--- src/main/java/hong/PdfToOtherTest.java
+++ src/main/java/hong/PdfToOtherTest.java
@@ -3,18 +3,18 @@ package hong;
 import com.spire.pdf.FileFormat;
 import com.spire.pdf.PdfDocument;
 
-public class PdfToOther {
+public class PdfToOtherTest {
 
 	private static final String PDF_FOLDER_PATH = "./config/pdf/";
-	
+
 	private static final String PDF_NM = "ASPHALTGOLD";
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("##### START ..");
 
 		//Create PDF document
-		PdfDocument pdf = new PdfDocument();	
+		PdfDocument pdf = new PdfDocument();
 
 		//Load the PDF document from disk.
 		pdf.loadFromFile(PDF_FOLDER_PATH + PDF_NM + ".PDF");
@@ -25,7 +25,8 @@ public class PdfToOther {
 		pdf.saveToFile(PDF_FOLDER_PATH + PDF_NM + ".HTML", FileFormat.HTML);
 		pdf.saveToFile(PDF_FOLDER_PATH + PDF_NM + ".POSTSCRIPT", FileFormat.POSTSCRIPT);
 		pdf.saveToFile(PDF_FOLDER_PATH + PDF_NM + ".PPTX", FileFormat.PPTX);
-		
+
 		System.out.println("##### END ..");
 	}
+
 }
